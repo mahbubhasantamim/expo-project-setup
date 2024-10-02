db1c3ee7476ab2c4a2212c73f054bf5513789144
mahbubhasantamim <mahbubhasan.pro@gmail.com>
2024-10-02 15:20:15 +0600

    project setup

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Text className="text-4xl text-red-500">Hello</Text>
+    </View>
+  );
+}
